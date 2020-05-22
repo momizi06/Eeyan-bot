8369957f8fddbeb0d56889402988dab90c69a910
犬走 椛 <momizi06.th10@gmail.com>
2020-05-22 15:12:45 +0900

    v0.0.2

--- eeyanBOT_temporary.py
+++ eeyanBOT_temporary.py
@@ -1,12 +1,12 @@
 
 # coding: utf-8
 
-import discord
 import discord.ext.commands
 import asyncio
 import random
 import sys
 import os
+
 
 bot = discord.ext.commands.Bot(command_prefix="e!")
 gamestatus = discord.Game("v0.0.1")
@@ -16,35 +16,27 @@ token = "unknown"
 # ええやん返信内容
 eeyanlist = ['ええやん', 'ええんちゃう？', 'おーええやん', '良き']
 
-# aboutコマンド用
-aboutembed = discord.Embed(title="このBotについて", description="ネタで作ってみました。")
-aboutembed.add_field(name="開発者", value="momizi06#5834")
 
 # 初期化と起動チェック用
-@bot.event()
+@bot.event
 async def on_ready():
-    print('We have logged by {0.users}'.format(bot))
+    print('We have logged by {0.user}'.format(bot))
     await bot.change_presence(status = discord.Status.idle,  activity = gamestatus)
 
 # ええやん返信用
-class Hensin(bot):
-    @bot.event()
-    async def on_message(self,message):
-
-        if message.author.bot:
-            return
-
-        def check(msg):
-            return msg.author == message.author
-
-        if message.content.startswith("ええやん"):
-            await message.channel.send(random.choice(eeyanlist))
+@bot.event
+async def on_message(message):
+    if message.content.startswith("ええやん"):
+        await message.channel.send(random.choice(eeyanlist))
 
 # aboutコマンド
-class BotCommand(bot):
-    @bot.command(name="about")
-    async def about(ctx):
-        await channel.send(embed=aboutembed)
+@bot.command(name="about")
+async def about(ctx):
+    # aboutコマンド用
+    aboutembed = discord.Embed(title="このBotについて", description="ネタで作ってみました。")
+    aboutembed.add_field(name="開発者", value="momizi06#5834")
+
+    await ctx.send(embed=aboutembed)
 
 # Botの稼働
 bot.run(token)
